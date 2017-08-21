b0999acf768f5ba072d7c8b29cab557df5065f2c
Yuri Aksenov <aksenov.mailbox@gmail.com>
2017-08-21 14:04:15 +0300

    стилизация выпадающего списка с помощью jquery form styler

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,9 @@
-$(".markets-list select").focus(function() {
-  $(".markets-list label").addClass("markets-list--focus");
-});
+/* Активация плагина jQuery Form Styler */
 
-$(".markets-list select").focusout(function() {
-  $(".markets-list label").removeClass("markets-list--focus");
-  $(".markets-list label").removeClass("markets-list--click");
-});
+(function($) {
+$(function() {
 
-$(".markets-list select").click(function() {
-  $(".markets-list label").toggleClass("markets-list--click");
+  $('input, select').styler();
+    
 });
+})(jQuery);
